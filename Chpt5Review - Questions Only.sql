39c81932117212fc29e30cecd862cc2280918e4f
DanielaTrapani <daniela@developersden.com>
2026-06-07 20:22:57 -0400

    Add files via upload

---
+++ Chpt5Review - Questions Only.sql
@@ -0,0 +1,31 @@
+USE AP
+-- 1.
+-- Create a query that counts unpaid invoices 
+-- and calculates the average of all invoices,
+-- and the total due for all invoices that have a balance.
+
+
+-- 2.
+-- Create a query that counts the number of invoices, 
+-- and finds the highest and lowest invoice totals 
+-- for invoices dated before 7/1/2022, 
+-- and after 7/1/2022. 
+
+
+-- 3.
+-- Create a query that counts the number of 
+-- invoices by vendor 
+
+
+-- 4.
+-- Create a query that counts the number of invoices,
+-- and calculates the average invoice total by vendor,
+-- for invoices with a total greater than $500.
+-- Sort the results by the number of invoices in 
+-- descending order.
+-- HINT: This query requires a join to the Vendors table
+-- to get the VendorName.
+
+
+
+
